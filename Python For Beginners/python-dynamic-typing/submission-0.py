a72af97bf8cb35a86c0c3ae1fd00735a37f89fa5
furqankhan <furqanmustafakhan@gmail.com>
2026-06-11 12:00:54 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 5
+print(type(variable))
+variable = 5.0
+
+print(type(variable))
+variable = True
+
+print(type(variable))
+variable = "True"
+
+print(type(variable))
+variable = [5, 4, 3, 2, 1]
+
+print(type(variable))
