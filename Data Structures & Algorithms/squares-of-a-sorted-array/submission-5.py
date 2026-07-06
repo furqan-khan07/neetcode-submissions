7e6433ecf1c2ff8f340823fa0a1ce7d7ce41e17e
furqankhan <furqanmustafakhan@gmail.com>
2026-07-06 11:28:05 -0400

    Add: squares-of-a-sorted-array - submission-5

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-5.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+
+        squares = []
+ 
+
+        for num in nums:
+            squares.append(num ** 2)
+
+        squares.sort()
+        return squares
+
+        
